71f85d2ce7d1327d9d09be16d7e5b4e96ccc6adb
Juan Jose Alvarez <jjalvarezv97@gmail.com>
2020-07-06 18:36:23 -0500

    Creacion de componentes, routing y barra de navegacion
    Solo HTML, sin estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Importo componentes
+import {AboutComponent} from './components/about/about.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {CreateComponent} from './components/create/create.component';
+import {ProjectsComponent} from './components/projects/projects.component';
+import {ErrorComponent} from './components/error/error.component';
 
-const routes: Routes = [];
+
+const appRoutes: Routes = [
+  {path : '', component : AboutComponent},
+  {path : 'about', component : AboutComponent},
+  {path : 'contact', component : ContactComponent},
+  {path : 'create', component : CreateComponent},
+  {path : 'projects', component : ProjectsComponent},
+  {path : '**', component : ErrorComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
